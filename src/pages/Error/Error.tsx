21b28c1514e7b37fca1140092bc6dcef95545a27
Mitra Singh <mitra.coder@gmail.com>
2024-03-13 12:17:28 -0400

    initial creation of pages for app, login and error page first to be added

---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Error = () => {
+	return (
+		<>
+			<div>Error</div>
+			<Link to="/">Return to login.</Link>
+		</>
+	);
+};
+
+export default Error;
