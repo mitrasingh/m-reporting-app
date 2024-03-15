21526a6ae021fc799e94b1deec001857db0abc31
Mitra Singh <mitra.coder@gmail.com>
2024-03-15 18:58:28 -0400

    setup component with react-hook-form, UI functionality is now live

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,34 @@
 import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type FormValues = {
+	email: string;
+	password: string;
+};
 
 const Login = () => {
 	// Will be placed into a CSS file later on
 	const containerStyle = {
 		marginTop: "10rem",
 		maxWidth: "650px",
+	};
+
+	// React Hook Form
+	const form = useForm<FormValues>({ mode: "onBlur" });
+	const { register, handleSubmit, formState } = form;
+	const { errors } = formState;
+	const emailRegex =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	const onSubmit: SubmitHandler<FormValues> = (data) => {
+		console.log("submitted");
+		console.log(data.email, data.password);
+	};
+
+	const testRegister = () => {
+		console.log(
+			"register user, this may button may not stay, refer to Anthony"
+		);
 	};
 
 	return (
@@ -17,25 +41,64 @@ const Login = () => {
 								<h6>Welcome to</h6>
 								<h2 className="fw-bold text-primary">M-Reporting</h2>
 							</div>
-							<Form>
+							<Form onSubmit={handleSubmit(onSubmit)} noValidate>
 								<Form.Group className="mb-4">
 									<Form.Label>Email</Form.Label>
-									<Form.Control></Form.Control>
+									<Form.Control
+										className="shadow-none fs-6"
+										type="text"
+										autoComplete="email"
+										placeholder="Enter email"
+										{...register("email", {
+											required: {
+												value: true,
+												message: "Email is required!",
+											},
+											pattern: {
+												value: emailRegex,
+												message: "Invalid email format!",
+											},
+										})}
+									/>
+									<p className="mt-2">{errors.email?.message}</p>
 								</Form.Group>
 
 								<Form.Group className="mb-3">
 									<Form.Label>Password</Form.Label>
-									<Form.Control></Form.Control>
+									<Form.Control
+										className="shadow-none fs-6"
+										type="password"
+										placeholder="Enter password"
+										{...register("password", {
+											required: {
+												value: true,
+												message: "Password is required!",
+											},
+										})}
+									/>
+									<p className="mt-2">{errors.password?.message}</p>
 								</Form.Group>
+								<Row>
+									<Col className="d-flex justify-content-end">
+										<Button
+											variant="secondary"
+											size="sm"
+											className="fw-bold text-light fs-6"
+											onClick={testRegister}
+										>
+											Register
+										</Button>
+										<Button
+											variant="primary"
+											className="ms-2 fw-bold text-light fs-6"
+											size="sm"
+											type="submit"
+										>
+											Login
+										</Button>
+									</Col>
+								</Row>
 							</Form>
-							<Row>
-								<Col className="d-flex justify-content-end">
-									<Button variant="secondary">Register</Button>
-									<Button variant="primary" className="ms-2">
-										Login
-									</Button>
-								</Col>
-							</Row>
 						</Col>
 					</Row>
 				</Card>
