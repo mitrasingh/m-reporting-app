7987b127a2b4e0d9e94ccfd310fbf612dc5e7244
Mitra Singh <mitra.coder@gmail.com>
2024-03-17 22:55:50 -0400

    toast component added to be used throughout application

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<App />
+			<Toaster position="top-right" />
 		</BrowserRouter>
 	</React.StrictMode>
 );
