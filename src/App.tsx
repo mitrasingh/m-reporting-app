4f089601a196c78d1e2597a9f001b497661ffc60
Mitra Singh <mitra.coder@gmail.com>
2024-03-22 23:24:43 -0400

    edit: removed container and react-bootstrap import

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
 import Login from "./pages/Login/Login";
 import Error from "./pages/Error/Error";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
 	return (
-		<Container className="py-4">
-			<Routes>
-				<Route path="/" element={<Login />} />
-				<Route path="*" element={<Error />} />
-			</Routes>
-		</Container>
+		<Routes>
+			<Route path="/" element={<Login />} />
+			<Route path="*" element={<Error />} />
+		</Routes>
 	);
 };
 
